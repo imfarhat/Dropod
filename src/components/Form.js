74a2cdbe1375a6d8e8ce1aaa78fd9b2fc32543e7
imfarhat <ifarhat.dev@gmail.com>
2024-03-20 21:53:09 +0530

    simple form add

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,32 +1,11 @@
-import { createRef } from "react";
 function Form() {
-  const fileInput = createRef();
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.set("file", fileInput.current.files[0]);
-    try {
-      const response = await fetch("/profile", {
-        method: "POST",
-        body: formData,
-      });
-
-      //const pasedResponse = await response.json();
-      if (response.ok) alert("File Uploaded");
-      else console.log("Something went wrong");
-    } catch (e) {
-      console.error(e.message);
-    }
-  };
-  return (
-    <>
-      <form onSubmit={handleSubmit} className="p-4">
-        <input type="file" name="file" id="file" ref={fileInput} required />
-        <button type="submit" className="py-1.5 px-4 border-2 rounded">
-          Submit
-        </button>
-      </form>
-    </>
-  );
+  <>
+    <form className="p-4">
+      <input type="file" name="file" id="file" required />
+      <button type="submit" className="py-1.5 px-4 border-2 rounded">
+        Submit
+      </button>
+    </form>
+  </>;
 }
 export default Form;
